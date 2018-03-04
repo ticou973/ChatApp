cc67f40dd1359939b61f3086ceacfa497ce12da6
Thierry Couilleault <thierryc973@gmail.com>
2018-03-04 01:24:22 +0100

    Fin 17

--- app/src/main/java/com/example/thierrycouilleault/chatapp/ProfileActivity.java
+++ app/src/main/java/com/example/thierrycouilleault/chatapp/ProfileActivity.java
@@ -1,23 +1,160 @@
 package com.example.thierrycouilleault.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView tv_user_id;
+    private TextView profileDisplayName;
+    private TextView profileDisplayStatus;
+    private TextView profileDisplayTotalFriends;
+    private  ImageView profileImage;
+    private Button profileSendRequestBtn, profileDeclineRequest;
+
+    private DatabaseReference mUsersDatabase;
+    private DatabaseReference mFriendsReqDatabase;
+    private FirebaseUser mCurrent_user;
+
+    private ProgressDialog mProgressDialog;
+    private String mCurrent_state;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //récupération des données de l'Intent
+        final String user_id=getIntent().getStringExtra("user_id");
 
-        tv_user_id = findViewById(R.id.tv_user_id);
+        //Déclaration de la database et positionnement dans la DB.
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        mFriendsReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friends_req");
 
-        String user_id=getIntent().getStringExtra("user_id");
+        mCurrent_state = "Not friends";
+        mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
 
-        tv_user_id.setText(user_id);
+
+        //findview
+        profileDisplayName = findViewById(R.id.profile_display_name);
+
+        profileDisplayName.setText(user_id);
+
+
+        profileDisplayStatus = findViewById(R.id.profile_status);
+
+        profileDisplayTotalFriends = findViewById(R.id.profile_totalFriends);
+
+        profileImage = findViewById(R.id.profile_image);
+
+
+        //gestion du bon d'envoi de la requête
+        profileSendRequestBtn = findViewById(R.id.profile_send_req_btn);
+        profileSendRequestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrent_state.equals("Not friends")){
+
+                    mFriendsReqDatabase.child(mCurrent_user.getUid()).child(user_id).child("request_type").setValue("sent")
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()){
+
+                                        mFriendsReqDatabase.child(user_id).child(mCurrent_user.getUid()).child("request_type").setValue("received")
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+
+                                                        Toast.makeText(ProfileActivity.this, "Requests sent successfull", Toast.LENGTH_SHORT).show();
+
+
+
+                                                    }
+                                                });
+
+
+
+                                    }else{
+
+                                        Toast.makeText(ProfileActivity.this, "Failed to send requests", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+
+                }
+
+
+            }
+        });
+
+
+        //gestion du bourton d'envoi du refus de requ^e
+        profileDeclineRequest = findViewById(R.id.profile_decline_request);
+        profileDeclineRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        //gestion de la ProgressDialog le temps de chargement des datas
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Loading User Data.. ");
+        mProgressDialog.setMessage("please wait while loading the user data ");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+
+        //gestion de l'évolution des données dans la DB.
+        mUsersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                profileDisplayName.setText(display_name);
+                profileDisplayStatus.setText(status);
+
+                Picasso.with(ProfileActivity.this).load(image).placeholder(R.drawable.default_avatar_square).into(profileImage);
+
+                mProgressDialog.dismiss();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 }
